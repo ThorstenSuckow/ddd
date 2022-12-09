14be0bc49c59aad82052c31d71563e8109496faa
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2022-12-09 17:23:41 +0100

    refactor: remove check if Deliveryhistory exists when adding event
    this is obviously not required anymore since the repeat-business-feature 
    introduced default empty DeliveryHistory

--- src/Model/Cargo.php
+++ src/Model/Cargo.php
@@ -1,5 +1,30 @@
 <?php
 
+/**
+ * DDD
+ * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/ThorstenSuckow/DDD
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 declare(strict_types=1);
 
 namespace DDD\Model;
@@ -26,6 +51,12 @@ class Cargo {
 
     private ?DeliverySpecification $deliverySpecification = null;
 
+    /**
+     * @todo can we avoid circular references?
+     * "Tracking is core to Cargo in this application. A history must
+     * refer to its subject."
+     * - [DDD, Evans, p. 170]
+     */
     private DeliveryHistory $deliveryHistory;
 
     private string $trackingId;
@@ -89,16 +120,6 @@ class Cargo {
      */
     public function handleEvent(HandlingEvent $event): static
     {
-        if (!$this->getDeliveryHistory()) {
-            /**
-             * @todo can we avoid circular references?
-             * "Tracking is core to Cargo in this application. A history must
-             * refer to its subject."
-             * - [DDD, Evans, p. 170]
-             */
-            $this->deliveryHistory = new DeliveryHistory($this);
-        }
-
         $this->deliveryHistory->addHandlingEvent($event);
     
         return $this;
