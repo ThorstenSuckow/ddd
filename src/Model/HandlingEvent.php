0ef510c3466aefdcb7de25e766bd8b701040efbb
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2022-12-09 17:21:48 +0100

    refactor: remove check if Deliveryhistory exists when adding event
    this is obviously not required anymore since the repeat-business-feature
    introduced default empty DeliveryHistory

--- src/Model/HandlingEvent.php
+++ src/Model/HandlingEvent.php
@@ -1,7 +1,32 @@
 <?php
 
+/**
+ * DDD
+ * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/ThorstenSuckow/DDD
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 declare(strict_types=1);
+
+namespace DDD\Model;
 
 use DateTime;
 
@@ -34,21 +59,44 @@ class HandlingEvent {
 
     private HandlingType $type;
 
-    private CarrierMovement $carrierMovement;
+    /**
+     * A HandlingEvent might have 1 or 0 CarrierMovements.
+     */
+    private ?CarrierMovement $carrierMovement = null;
 
     private string $trackingId;
 
+
+    /**
+     * Constructor.
+     * 
+     * @param string $trackingId Passing the tracking id of a Cargo here
+     * so that no (possible expensive) reconstitution of a Cargo instance is 
+     * necessary
+     * @param DateTime $completionTime
+     * @param HandlingType $type;
+     * 
+     */
     public function __construct(
-        Cargo $cargo,
-        CarrierMovement $carrierMovement,
+        string $trackingId,
         DateTime $completionTime, 
-        HandlingType $type)
-    {
-        $this->trackingId = $cargo->getTrackingId();
-        $this->carrierMovement = $carrierMovement;
+        HandlingType $type
+    ) {
+        $this->trackingId = $trackingId;
         $this->completionTime = $completionTime;
         $this->type = $type;    
     }
 
 
+    public function setCarrierMovement(CarrierMovement $carrierMovement): static 
+    {
+        $this->carrierMovement = $carrierMovement;
+        return $this;
+    }
+
+
+    public function getCarrierMovement(): ?CarrierMovement
+    {
+        return $this->carrierMovement;
+    }
 }
